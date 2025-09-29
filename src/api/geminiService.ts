e622a27dea74312935082a5ec2b02bb00a1c438a
Chamu Mutezva <ckmutezva@gmail.com>
2025-09-29 22:24:53 +0200

    fix: trial server functions (deployed) pointing to local dev url

--- src/api/geminiService.ts
+++ src/api/geminiService.ts
@@ -4,7 +4,8 @@
  */
 
 // WARNING: This URL must match the port and endpoint of your running Node.js server
-const PROXY_API_URL = "http://localhost:3000/api/advice"; 
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+const PROXY_API_URL = `${API_BASE_URL}/api/advice`
 
 interface AdvicePayload {
     userPrompt: string;
